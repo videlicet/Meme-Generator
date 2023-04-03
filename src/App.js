360a30964eb5ef6c2708024508decec0384e9a60
Christoph Bornemann <bornemch@gmail.com>
2023-04-03 15:02:54 +0200

    Basic functions implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [memes, setMemes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [textOne, setTextOne] = useState('');
+  const [textTwo, setTextTwo] = useState('');
+  const [currentMeme, setCurrentMeme] = useState(0);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.imgflip.com/get_memes`
+        );
+        console.log(response.data)
+        setMemes(response.data.data.memes);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setMemes(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log();
+    setTextOne('');
+    setTextTwo('');
+  }
+
+  function handleChangeOne(event) {
+    setTextOne(event.target.value);
+  }
+  function handleChangeTwo(event) {
+    setTextTwo(event.target.value);
+  }
+
+  function onPrevious() {
+    setCurrentMeme(prev => prev-1);
+  }
+
+  function onNext() {
+    setCurrentMeme(prev => prev+1);
+  }
+
+  function onRandom() {
+    setCurrentMeme(Math.floor(Math.random() * 100));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-container">
+      <h1>Meme Generator</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label for='above'>Text above: </label>
+          <input onChange={handleChangeOne} name='above' type='text' value={textOne}></input><span> Meme text: {textOne}</span>
+        </div>
+        <div>
+        <label for='below'>Text below: </label>
+        <input onChange={handleChangeTwo} name='below' type='text' value={textTwo}></input><span> Meme text: {textTwo}</span><br/>
+        </div>
+        <input type='submit' value='generate'></input>
+      </form>
+      <div>
+        {loading && <span>Loading</span>}
+        {memes[currentMeme] && <img src={memes[currentMeme].url} alt=""/>}
+      </div>
+      <div className='navigation'>
+        <div>
+          {currentMeme-1 !== -1 && <button onClick={onPrevious}>previous</button>}
+          {currentMeme+1 !== 100 &&<button onClick={onNext}>next</button>}
+        </div>
+        <div>
+          <button onClick={onRandom}>random</button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+/*     {loading && <span>Loading ...</span>}*/
