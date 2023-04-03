0a373a3c84720281bf99d7506add7e841e0aeb41
Christoph Bornemann <bornemch@gmail.com>
2023-04-03 17:53:46 +0200

    Improved styling; complete functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import domtoimage from 'dom-to-image';
 import axios from 'axios';
 
 function App() {
@@ -10,28 +11,56 @@ function App() {
   const [textTwo, setTextTwo] = useState('');
   const [currentMeme, setCurrentMeme] = useState(0);
 
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.imgflip.com/get_memes`
+      );
+      console.log(response.data)
+      console.log(response.data.data.memes)
+      setMemes(response.data.data.memes);
+      setError(null);
+    } catch (err) {
+      setError(err.message);
+      setMemes(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.imgflip.com/get_memes`
-        );
-        console.log(response.data)
-        setMemes(response.data.data.memes);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setMemes(null);
-      } finally {
-        setLoading(false);
-      }
-    };
     getData();
   }, []);
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log();
+    axios.post(
+			"https://api.imgflip.com/caption_image",
+			{
+				form: {
+					template_id: '181913649',
+					username: 'user99990',
+					password: 'Lgn7H96ReMmZtmc',
+					text0: 'hello',
+					text1: 'there',
+				},
+			}
+		)
+    // axios.post('https://api.imgflip.com/caption_image', {
+    //   username: 'user99990',
+    //   password: 'Lgn7H96ReMmZtmc',
+    //   template_id: '181913649', //memes[currentMeme].id
+    //   text0: textOne,
+    //   text1: textTwo
+    // })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
     setTextOne('');
     setTextTwo('');
   }
@@ -39,40 +68,98 @@ function App() {
   function handleChangeOne(event) {
     setTextOne(event.target.value);
   }
+
   function handleChangeTwo(event) {
     setTextTwo(event.target.value);
   }
 
   function onPrevious() {
+    getData();
     setCurrentMeme(prev => prev-1);
+    setTextOne('');
+    setTextTwo('');
   }
 
   function onNext() {
+    getData();
     setCurrentMeme(prev => prev+1);
+    setTextOne('');
+    setTextTwo('');
   }
 
   function onRandom() {
+    getData();
     setCurrentMeme(Math.floor(Math.random() * 100));
+    setTextOne('');
+    setTextTwo('');
+  }
+
+  function onUpload() {
+    if(document.getElementById('input').files[0]) {
+    const fileURL = URL.createObjectURL(document.getElementById('input').files[0]);
+    console.log(fileURL);
+    setMemes([{url: fileURL}]);
+    }
+  }
+
+  function onDownload() {
+    domtoimage.toJpeg(document.getElementById('meme'), { quality: 0.95 })
+    .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = 'my-meme.jpeg';
+        link.href = dataUrl;
+        link.click();
+    });
   }
 
   return (
     <div className="page-container">
       <h1>Meme Generator</h1>
+
       <form onSubmit={handleSubmit}>
-        <div>
-          <label for='above'>Text above: </label>
-          <input onChange={handleChangeOne} name='above' type='text' value={textOne}></input><span> Meme text: {textOne}</span>
+        <div className='text-input'>
+          <label htmlFor='above'>Text above: </label>
+          <input onChange={handleChangeOne} name='above' type='text' value={textOne}></input>
         </div>
-        <div>
-        <label for='below'>Text below: </label>
-        <input onChange={handleChangeTwo} name='below' type='text' value={textTwo}></input><span> Meme text: {textTwo}</span><br/>
+        <div className='text-input'>
+        <label htmlFor='below'>Text below: </label>
+        <input onChange={handleChangeTwo} name='below' type='text' value={textTwo}></input><br/>
         </div>
         <input type='submit' value='generate'></input>
       </form>
-      <div>
-        {loading && <span>Loading</span>}
-        {memes[currentMeme] && <img src={memes[currentMeme].url} alt=""/>}
+
+      <div className='interaction'>
+        <div className='upload'>    
+          <label for="file-upload" class="custom-file-upload" >
+            Upload your own template:
+            <br/>
+            <input name='file-upload' type="file" id="input" multiple />
+          </label>
+          <button onClick={onUpload}>upload</button>
+        </div>
+
+        <div className='download'>
+          <label for="file-download" class="custom-file-download" >
+            Download your meme:
+            <br/>
+            <button  onClick={onDownload} name='file-download'>download</button>
+          </label>    
+
+        </div>
       </div>
+
+      
+      {loading && <span>Loading</span>}
+        {memes[currentMeme] && <div className='border'>
+        <div className='meme' id='meme'>
+          <img src={memes[currentMeme].url} alt=''/>
+          <div className='overlay'>
+          <span className='text'>{textOne}</span>
+          <span className='text'>{textTwo}</span>
+          </div>
+        </div>
+        </div>}
+
       <div className='navigation'>
         <div>
           {currentMeme-1 !== -1 && <button onClick={onPrevious}>previous</button>}
@@ -87,6 +174,3 @@ function App() {
 }
 
 export default App;
-
-
-/*     {loading && <span>Loading ...</span>}*/
